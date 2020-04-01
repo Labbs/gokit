40eb7a0d251008b59e3a6f1f42c20d37d3412bfd
Labbs <moutonjeremy@labbs.fr>
2020-04-01 15:37:02 +0000

    update cfg

--- cfg/cfg.go
+++ cfg/cfg.go
@@ -2,18 +2,18 @@ package cfg
 
 import (
 	"log"
+	"time"
 
 	"github.com/hashicorp/consul/api"
 	"gopkg.in/yaml.v2"
 )
 
 var (
-	Config map[string][]string
+	Config map[string]interface{}
 )
 
 func InitCfg(address string, debug bool, service string) {
 	for {
-		t := make(map[string][]string)
 		c, _ := api.NewClient(&api.Config{Address: address})
 		kv := c.KV()
 		consulConfig, _, err := kv.Get("config/"+service, nil)
@@ -22,16 +22,17 @@ func InitCfg(address string, debug bool, service string) {
 		}
 
 		if consulConfig != nil {
-			err = yaml.Unmarshal([]byte(consulConfig.Value), &t)
+			err = yaml.Unmarshal([]byte(consulConfig.Value), &Config)
 			if err != nil {
 				log.Fatalf(err.Error())
 			}
-			Config = t
 			if debug {
-				log.Printf("config reload")
+				log.Printf("config reloaded")
 			}
 		} else {
 			log.Printf("config empty")
 		}
+
+		time.Sleep(30 * time.Second)
 	}
 }
