2aedd842b867b8009ce6426ade46eda599d1173f
Labbs <moutonjeremy@labbs.fr>
2020-04-01 22:45:32 +0000

    Add logger

--- cfg/cfg.go
+++ cfg/cfg.go
@@ -1,38 +1,40 @@
 package cfg
 
 import (
-	"log"
 	"time"
 
 	"github.com/hashicorp/consul/api"
+	"go.uber.org/zap"
 	"gopkg.in/yaml.v2"
 )
 
 var (
 	// Config - var contains configuration from consul
 	Config map[string]interface{}
+	Logger *zap.Logger
 )
 
 // InitCfgLoop - get configuration from consul
 func InitCfgLoop(address string, debug bool, service string) {
+	Logger, _ = zap.NewProduction()
 	for {
 		c, _ := api.NewClient(&api.Config{Address: address})
 		kv := c.KV()
 		consulConfig, _, err := kv.Get("config/"+service, nil)
 		if err != nil {
-			log.Fatalf(err.Error())
+			Logger.Fatalf(err.Error())
 		}
 
 		if consulConfig != nil {
 			err = yaml.Unmarshal([]byte(consulConfig.Value), &Config)
 			if err != nil {
-				log.Fatalf(err.Error())
+				Logger.Fatalf(err.Error())
 			}
 			if debug {
-				log.Printf("config reloaded")
+				Logger.Info("config reloaded")
 			}
 		} else {
-			log.Printf("config empty")
+			Logger.Info("config reloaded")
 		}
 
 		time.Sleep(30 * time.Second)
@@ -41,21 +43,22 @@ func InitCfgLoop(address string, debug bool, service string) {
 
 // InitCfg - get configuration from consul
 func InitCfg(address string, debug bool, service string) {
+	Logger, _ = zap.NewProduction()
 	c, _ := api.NewClient(&api.Config{Address: address})
 	kv := c.KV()
 	consulConfig, _, err := kv.Get("config/"+service, nil)
 	if err != nil {
-		log.Fatalf(err.Error())
+		Logger.Fatalf(err.Error())
 	}
 	if consulConfig != nil {
 		err = yaml.Unmarshal([]byte(consulConfig.Value), &Config)
 		if err != nil {
-			log.Fatalf(err.Error())
+			Logger.Fatalf(err.Error())
 		}
 		if debug {
-			log.Printf("config reloaded")
+			Logger.Info("config reloaded")
 		}
 	} else {
-		log.Printf("config empty")
+		Logger.Info("config reloaded")
 	}
 }
