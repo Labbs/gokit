1fa27ce6048b70c174de3f9ffb0ac58d9d9bda39
Labbs <moutonjeremy@labbs.fr>
2020-04-06 20:50:48 +0200

    add debug mode for xorm

--- database/database.go
+++ database/database.go
@@ -2,18 +2,21 @@ package database
 
 import (
 	"github.com/go-xorm/xorm"
+	_ "github.com/lib/pq"
 
 	"github.com/Labbs/gokit/cfg"
 )
 
 // InitEngine - init xorm engine database
-func InitEngine(engine string, dataSource []string, tables []interface{}) *xorm.EngineGroup {
+func InitEngine(engine string, dataSource []string, tables []interface{}, debug bool) *xorm.EngineGroup {
 	cfg.Logger.Info("create database engine")
 	e, err := xorm.NewEngineGroup(engine, dataSource)
 	if err != nil {
 		cfg.Logger.Fatal(err.Error())
 	}
 
+	e.ShowSQL(debug)
+	e.ShowExecTime(debug)
 	cfg.Logger.Info("create table")
 	err = e.Sync2(tables...)
 	if err != nil {
